a82dd9d5d4ca817a044eaff70fadd762848ec2a9
Oscar David  Rodriguez Marin <1719110304@utectulancingo.edu.mx>
2021-01-28 20:17:21 +0000

    Terminado

--- google_books/book.py
+++ google_books/book.py
@@ -1,7 +1,25 @@
 import requests
 import json
 
-resultado = requests.get("https://www.googleapis.com/books/v1/volumes?q=harry+potter")
+result = requests.get("https://www.googleapis.com/books/v1/volumes?q=sinsajo")
 
-print (resultado.text)
+book = result.json()
+
+items = book["items"]
+
+encoded = json.dumps(items)
+decoded = json.loads(encoded)
+#print(decoded[0]["volumeInfo"]["infoLink"])
+print(decoded[0]["volumeInfo"]["authors"])
+
+autor=str(decoded[0]["volumeInfo"]["authors"])
+print(autor)
+autor.replace("[","")
+autor.replace("]","")
+print(decoded[0]["volumeInfo"]["imageLinks"]["smallThumbnail"])
+
+#print(decoded[0]["volumeInfo"]["smallThumbnail"])
+#print (autor)
+
+print (result.text)
 
--- mvc/index.py
+++ mvc/index.py
@@ -6,12 +6,13 @@ render= web.template.render("mvc/")
 
 class Index():
   def GET(self):
-    return render.index()
+    datos=None
+    return render.index(datos)
 
   def POST(self)  :
     form=web.input()
     nombre_libro=form.nombre_libro
-
+    
     resultado = requests.get("https://www.googleapis.com/books/v1/volumes?q="+nombre_libro)
 
     book = resultado.json()
@@ -22,7 +23,19 @@ class Index():
     decoded = json.loads(encoded)
 
     url = decoded[0]["volumeInfo"]["infoLink"]
-
+    url_imagen=decoded[0]["volumeInfo"]["imageLinks"]["smallThumbnail"]
+    nombre_autor=str(decoded[0]["volumeInfo"]["authors"])
     link ="<a target='blank' href='"+url+"'>"+nombre_libro+"</a>"
+    
+    imagen ="<img src='"+url_imagen+"'/>"
+    autor="<label>'"+nombre_autor+"'</label>"
 
-    return link
+    datos={
+      
+      "nombre_libro":"Nombre del libro: "+nombre_libro,
+      "imagen":imagen,
+      "autor":"Autor: "+autor,
+      "url":"Link de compra: "+link
+
+    }
+    return render.index(datos)
